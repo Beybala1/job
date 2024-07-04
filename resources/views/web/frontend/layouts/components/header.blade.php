a32528a0147022bc2c1542ce6a4d6f7df7727cd7
eejIgmWY0qgr <b.muxtarov@ishesabi.com>
2024-07-04 21:50:07 +0400

    Translatable package and header lang select added

--- resources/views/web/frontend/layouts/components/header.blade.php
+++ resources/views/web/frontend/layouts/components/header.blade.php
@@ -16,29 +16,32 @@
                             <div class="main-menu">
                                 <nav class="d-none d-lg-block">
                                     <ul id="navigation">
-                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="index.html">Homeho</a></li>
                                         <li><a href="index.html">Home</a></li>
                                         <li><a href="index.html">Home</a></li>
                                         <li><a href="index.html">Home</a></li>
                                         <li><a href="contact.html">Contact</a></li>
-                                        {{-- <li><a href="#">Page</a>
-                                            <ul class="submenu">
-                                                <li><a href="blog.html">Blog</a></li>
-                                                <li><a href="single-blog.html">Blog Details</a></li>
-                                                <li><a href="elements.html">Elements</a></li>
-                                                <li><a href="job_details.html">job Details</a></li>
-                                            </ul>
-                                        </li> --}}
                                     </ul>
                                 </nav>
-                            </div>          
+                            </div>                           
                             <!-- Header-btn -->
                             <div class="header-btn d-none f-right d-lg-block">
                                 <a href="#" class="btn head-btn1">Register</a>
                                 <a href="#" class="btn head-btn2">Login</a>
                             </div>
+
+                            <!-- Lang select -->
+                            <div class="language-select-wrapper d-none d-lg-block">
+                                <select class="language-select">
+                                    <option value="en">English</option>
+                                    <option value="es">Español</option>
+                                    <option value="fr">Français</option>
+                                    <option value="de">Deutsch</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
+                    
                     <!-- Mobile Menu -->
                     <div class="col-12">
                         <div class="mobile_menu d-block d-lg-none"></div>

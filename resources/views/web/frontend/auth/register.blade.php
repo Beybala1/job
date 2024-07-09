15aabc316211740e2e42bb0fa366cdbb4f71c980
eejIgmWY0qgr <b.muxtarov@ishesabi.com>
2024-07-09 23:55:52 +0400

    ui package

--- resources/views/web/frontend/auth/register.blade.php
+++ resources/views/web/frontend/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('web.frontend.layouts.app')
 
 @section('content')
 <div class="container">
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => '{locale}', 'middleware' => 'setlocale'], function() {
+Route::group(['prefix' => '{locale}', 'middleware' => ['setlocale']], function() {
     Route::get('/', [HomeController::class, 'index'])->name('home');
 });
+
+// Heç bir route görsənmir
+// 'prefix' => '{locale}', buna görədir
+Auth::routes();
 
 Route::get('/', function () {
     return redirect(app()->getLocale());
@@ -14,10 +19,3 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('web.backend.index');
 });
-
-
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
